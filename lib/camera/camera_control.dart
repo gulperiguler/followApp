fdcf0e0729bd7c0d104ce2c0929b7de4c9dd7dee
gulperi <gulperiguler@gmail.com>
2021-02-07 22:38:13 +0300

    camera

---
+++ lib/camera/camera_control.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+// List<CameraDescription> cameras;
+
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   cameras = await availableCameras();
+//   runApp(CameraControl());
+// }
+
+class CameraControl extends StatefulWidget {
+  List<CameraDescription> cameras;
+  CameraControl(List<CameraDescription> cameras) {
+    this.cameras = cameras;
+  }
+  @override
+  _CameraControlAppState createState() => _CameraControlAppState();
+}
+
+class _CameraControlAppState extends State<CameraControl> {
+  CameraController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(widget.cameras[1], ResolutionPreset.medium);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
+    return AspectRatio(aspectRatio: controller.value.aspectRatio, child: CameraPreview(controller));
+  }
+}
